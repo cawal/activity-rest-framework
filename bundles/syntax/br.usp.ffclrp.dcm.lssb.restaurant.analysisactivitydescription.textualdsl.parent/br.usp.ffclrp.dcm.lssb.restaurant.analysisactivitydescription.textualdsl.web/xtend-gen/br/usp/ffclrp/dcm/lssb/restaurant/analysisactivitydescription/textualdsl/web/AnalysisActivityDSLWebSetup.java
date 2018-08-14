645514e3ade254e6b9b065dc80915373bf1388cf
cawal <ricardo.cawal@gmail.com>
2018-08-14 17:19:17 -0300

    Need to remove xtendbin from git

--- bundles/syntax/br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.parent/br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.web/xtend-gen/br/usp/ffclrp/dcm/lssb/restaurant/analysisactivitydescription/textualdsl/web/AnalysisActivityDSLWebSetup.java
+++ bundles/syntax/br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.parent/br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.web/xtend-gen/br/usp/ffclrp/dcm/lssb/restaurant/analysisactivitydescription/textualdsl/web/AnalysisActivityDSLWebSetup.java
@@ -3,19 +3,24 @@
  */
 package br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.web;
 
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.AnalysisActivityDSLRuntimeModule;
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.AnalysisActivityDSLStandaloneSetup;
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.ide.AnalysisActivityDSLIdeModule;
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.textualdsl.web.AnalysisActivityDSLWebModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.eclipse.xtext.util.Modules2;
+
 /**
  * Initialization support for running Xtext languages in web applications.
  */
 @SuppressWarnings("all")
-public class AnalysisActivityDSLWebSetup /* implements AnalysisActivityDSLStandaloneSetup  */{
+public class AnalysisActivityDSLWebSetup extends AnalysisActivityDSLStandaloneSetup {
   @Override
-  public /* Injector */Object createInjector() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method or field Guice is undefined"
-      + "\nThe method or field Modules2 is undefined"
-      + "\nAnalysisActivityDSLRuntimeModule cannot be resolved."
-      + "\nAnalysisActivityDSLIdeModule cannot be resolved."
-      + "\ncreateInjector cannot be resolved"
-      + "\nmixin cannot be resolved");
+  public Injector createInjector() {
+    AnalysisActivityDSLRuntimeModule _analysisActivityDSLRuntimeModule = new AnalysisActivityDSLRuntimeModule();
+    AnalysisActivityDSLIdeModule _analysisActivityDSLIdeModule = new AnalysisActivityDSLIdeModule();
+    AnalysisActivityDSLWebModule _analysisActivityDSLWebModule = new AnalysisActivityDSLWebModule();
+    return Guice.createInjector(Modules2.mixin(_analysisActivityDSLRuntimeModule, _analysisActivityDSLIdeModule, _analysisActivityDSLWebModule));
   }
 }
