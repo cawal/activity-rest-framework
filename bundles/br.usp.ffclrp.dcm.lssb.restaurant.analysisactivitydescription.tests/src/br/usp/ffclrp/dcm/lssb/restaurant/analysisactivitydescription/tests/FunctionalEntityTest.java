e0d1dfadc5fb29bbc678d38032f5c08e59373a6b
cawal <ricardo.cawal@gmail.com>
2018-09-24 14:08:10 -0300

    Tool -> FunctionalEntity, Only one constraint

---
+++ bundles/br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.tests/src/br/usp/ffclrp/dcm/lssb/restaurant/analysisactivitydescription/tests/FunctionalEntityTest.java
@@ -0,0 +1,36 @@
+/**
+ */
+package br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.tests;
+
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.FunctionalEntity;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Functional Entity</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class FunctionalEntityTest extends IdentifiableElementTest {
+
+	/**
+	 * Constructs a new Functional Entity test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public FunctionalEntityTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Functional Entity test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected FunctionalEntity getFixture() {
+		return (FunctionalEntity)fixture;
+	}
+
+} //FunctionalEntityTest
