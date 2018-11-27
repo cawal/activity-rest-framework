bc75c8c26adc54972a9a976025967800170fe3a5
cawal <ricardo.cawal@gmail.com>
2018-11-27 18:47:08 -0200

    new hypermedia controls

--- activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/rest/ResourceRelations.java
+++ activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/rest/ResourceRelations.java
@@ -3,7 +3,7 @@ package br.usp.ffclrp.dcm.lssb.activityrest.rest;
 public class ResourceRelations {
 	public static final String SELF = "self";
 	public static final String ROOT_2_NEW_ANALYSES_COLLECTION =
-			"createAnalysis";
+			"create-instance";
 	public static final String ANALYSYS_PARAMETERS_COLLECTION =
 			"parameters";
 	public static final String ANALYSYS_INPUT_DATASETS_COLLECTION =
@@ -13,7 +13,7 @@ public class ResourceRelations {
 	public static final String DATASET_TO_ITEMS_GET_RELATION =
 			"item";
 	public static final String SUBMIT_RELATION =
-			"startProcessing";
+			"start-execution";
 	public static final String SWAGGER_JSON = "openapiDocumentationJson";
 	public static final String SWAGGER_YAML = "openapiDocumentationYaml";
 	public static final String ANALYSYS_ERROR_REPORT = "error-report";
