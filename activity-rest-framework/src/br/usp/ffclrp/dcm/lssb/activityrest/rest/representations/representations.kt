c1877efafd4a881a6d0a2d1753ff97b80998c6df
CaWaL <ricardo.cawal@gmail.com>
2018-11-28 00:06:12 -0200

    data modifier to representation classes in kotlin

--- activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/rest/representations/representations.kt
+++ activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/rest/representations/representations.kt
@@ -2,7 +2,7 @@ package br.usp.ffclrp.dcm.lssb.activityrest.rest.representations
 
 import java.net.URI
 
-class AnalysisActivityRepresentation(
+data class AnalysisActivityRepresentation(
 	val id: String,
 	var state: AnalysisActivityStateRepresentation,
 	val errorReport: String?
@@ -17,25 +17,25 @@ enum class AnalysisActivityStateRepresentation {
 	REMOVED
 }
 
-public class JobInstanceRepresentation(
+data class JobInstanceRepresentation(
 	val id: String,
 	var state: AnalysisActivityStateRepresentation,
 	var errorReport: String
 )
 
-public class ParameterRepresentation(
+data class ParameterRepresentation(
 	val name: String,
 	var value: List<Any>
 )
 
 
-class FileRepresentation(
+data class FileRepresentation(
 	val name: String,
 	val content: String,
 	val contentType: String?
 )
 
-class HypermediaLink(
+data class HypermediaLink(
 	val rel: String,
 	val uri: URI,
 	val type: String?
