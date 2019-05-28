03563a58b1d057cc19b9f564ba40b3cd8201ada1
cawal <ricardo.cawal@gmail.com>
2019-05-28 16:59:08 -0300

    WIP Updating archetype

--- framework/archetype/src/main/resources/archetype-resources/src/__packageInPathFormat__/ApplicationConfig.java
+++ framework/archetype/src/main/resources/archetype-resources/src/__packageInPathFormat__/ApplicationConfig.java
@@ -3,20 +3,68 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
+import br.usp.ffclrp.dcm.lssb.activityrest.dao.FileSystemActivityRepository;
+import br.usp.ffclrp.dcm.lssb.activityrest.rest.ActivityRestConfig;
 import br.usp.ffclrp.dcm.lssb.activityrest.rest.BasicApplicationJAXRSConfig;
-import io.swagger.jaxrs.config.BeanConfig;
+import br.usp.ffclrp.dcm.lssb.activityrest.util.ModelsService;
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.Activity;
 
 @ApplicationPath("/")
 public class ApplicationConfig extends BasicApplicationJAXRSConfig {
 	
+	final String ACTIVITYREST_PROPERTIES = "activityrest.properties";
+	static final String ACTIVITY_DESCRIPTION = "activity.aadl";
+
+	ActivityRestConfig config;
+	
 	public ApplicationConfig() {
 		super();
-		
+
+		this.config = new ActivityRestConfig();
+		try {
+			Activity activityModel = ModelsService.retrieveAADLModel(
+					this.getClass().getResourceAsStream(
+							ACTIVITY_DESCRIPTION));
+			
+			this.config.setActivityModel(activityModel);
+			Properties deploymentProperties = new Properties();
+			deploymentProperties.load(
+					this.getClass().getResourceAsStream(ACTIVITYREST_PROPERTIES));
+			
+			File nonExecutedActivityStorage = 
+					new File(deploymentProperties.getProperty("activityrest.repositories.nonExecuted"));
+			this.config.setNewAnalysisRepository(
+					new FileSystemActivityRepository(nonExecutedActivityStorage, activityModel));
+			
+			File runningActivityStorage = 
+					new File(deploymentProperties.getProperty("activityrest.repositories.running"));
+			this.config.setRunningAnalysisRepository(
+					new FileSystemActivityRepository(runningActivityStorage, activityModel));
+			
+			File failedActivityStorage = 
+					new File(deploymentProperties.getProperty("activityrest.repositories.failed"));
+			this.config.setFailedAnalysisRepository(
+					new FileSystemActivityRepository(failedActivityStorage, activityModel));
+			
+			File succeededActivityStorage = 
+					new File(deploymentProperties.getProperty("activityrest.repositories.succeeded"));
+			this.config.setSuccededAnalysisRepository(
+					new FileSystemActivityRepository(succeededActivityStorage, activityModel));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -24,9 +72,15 @@ public class ApplicationConfig extends BasicApplicationJAXRSConfig {
 		Set<Class<?>> resources = super.getClasses();
 		
 		// add here all classes with JAX-RS annotations
-		resources.add(${package}.RootResource.class);
 		
 		return resources;
+	}
+	
+	@Override
+	public Map<String, Object> getProperties() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("activityrest.config", this.config);
+		return map;
 	}
 	
 }
