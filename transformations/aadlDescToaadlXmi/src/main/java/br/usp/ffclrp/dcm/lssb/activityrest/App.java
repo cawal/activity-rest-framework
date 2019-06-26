bb4f2175f7a3204d817a63946e0b6737ffa86855
cawal <ricardo.cawal@gmail.com>
2019-06-26 16:44:01 -0300

    CLI working over directories

--- transformations/aadlDescToaadlXmi/src/main/java/br/usp/ffclrp/dcm/lssb/activityrest/App.java
+++ transformations/aadlDescToaadlXmi/src/main/java/br/usp/ffclrp/dcm/lssb/activityrest/App.java
@@ -2,11 +2,8 @@ package br.usp.ffclrp.dcm.lssb.activityrest;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.concurrent.Callable;
 
 import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.Activity;
@@ -15,23 +12,27 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 /**
- * Reads a AADL description and transforms it in a AADL XMI file.
+ * Reads AADL descriptions and transforms then in AADL XMI files.
  * @author cawal
  *
  */
-@Command(description = "Reads a AADL description and transforms it in a AADL XMI file.")
+@Command(description = "Reads a directory, look for AADL description by the"
++ " '.aadl' extension  and transforms each in a XMI file.")
 public class App implements Callable<Integer> {
 
 	@Parameters(index = "0",
-			description = "The path of a directory were the AADL descriptions will be looked for.",
+			description = "The path of a directory were the AADL descriptions (*.aadl) will be looked for.",
 			paramLabel = "input_directory")
 	private File inputAADLDirectory;
 	
 
 	@Parameters(index = "1",
-			description = "The path of a directory were the AADL descriptions will be looked for.",
+			description = "The path of a directory were the XMI files will written.",
 			paramLabel = "output_directory")
 	private File outputXMIDirectory;
+	
+	
+	final String aadl_extension = "aadl";
 	
 	
 	public static void main(String[] args) throws Exception {
@@ -41,15 +42,16 @@ public class App implements Callable<Integer> {
 
 	@Override
 	public Integer call() throws Exception {
+		
 		if(inputAADLDirectory.exists() && inputAADLDirectory.isDirectory()) {
-
+		
 			outputXMIDirectory.mkdirs();
-			
 			Arrays.asList(inputAADLDirectory.listFiles()).stream()
-				.filter(f -> f.getName().endsWith("aadl"))
+				.filter(f -> f.getName().endsWith(aadl_extension))
 				.forEachOrdered(this::transformFile);
 			
 			return 0;
+		
 		} else {
 			return 1;
 		}
@@ -60,8 +62,10 @@ public class App implements Callable<Integer> {
 		try (InputStream aadlSource = new FileInputStream(input);) {
 		
 			Activity aadlModel = ModelsService.retrieveAADLModel(aadlSource);
+			String newFileName = input.getName()
+					.replaceAll("\\."+aadl_extension, ".xmi");
 			String outputPath = outputXMIDirectory.getPath() 
-					+"/"+ input.getName().replaceAll("\\.aadl", ".xmi");
+					+"/" + newFileName;
 			
 			ModelsService.writeAADLModelToXmi(aadlModel, outputPath);
 		} catch (Exception e) {
--- transformations/aadlXmiToJsonOpenApi/src/main/java/br/usp/ffclrp/dcm/lssb/activityrest/App.java
+++ transformations/aadlXmiToJsonOpenApi/src/main/java/br/usp/ffclrp/dcm/lssb/activityrest/App.java
@@ -1,7 +1,10 @@
 package br.usp.ffclrp.dcm.lssb.activityrest;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 import org.eclipse.emf.common.util.URI;
@@ -9,7 +12,7 @@ import org.eclipse.emf.common.util.URI;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import edu.uoc.som.openapi.API;
+import br.usp.ffclrp.dcm.lssb.restaurant.analysisactivitydescription.Activity;
 import edu.uoc.som.openapi.Root;
 import edu.uoc.som.openapi.io.ExporterBuilder;
 import edu.uoc.som.openapi.io.OpenAPIExporter;
@@ -22,13 +25,28 @@ import picocli.CommandLine.Parameters;
  */
 public class App implements Callable<Integer> {
 	
-	@Parameters(index = "0", 
-			description = "The OpenAPI XMI file to convert.")
-	private String inputFilePath;
+	/*
+	 * @Parameters(index = "0",
+	 * description = "The OpenAPI XMI file to convert.")
+	 * private String inputFilePath;
+	 * 
+	 * @Parameters(index = "1",
+	 * description = "The path for the resulting OpenAPI JSON file.")
+	 * private String outputJsonFilePath;
+	 * 
+	 */
+	
+	@Parameters(index = "0",
+			description = "The path of a directory were the AADL XMI "
+					+ "(*.xmi) will be looked for.",
+			paramLabel = "input_directory")
+	private File inputAADLDirectory;
 	
 	@Parameters(index = "1",
-			description = "The path for the resulting OpenAPI JSON file.")
-	private String outputJsonFilePath;
+			description = "The path of a directory were the Open API Json "
+					+ "files will be written.",
+			paramLabel = "output_directory")
+	private File outputJSONDirectory;
 	
 	public static void main(String[] args) throws Exception {
 		int exitCode = new CommandLine(new App()).execute(args);
@@ -38,19 +56,44 @@ public class App implements Callable<Integer> {
 	@Override
 	public Integer call() throws Exception {
 		
-		URI resourceUri = URI.createURI(inputFilePath);
-		Root root = ModelsService.retrieveOpenApiRootFromXmi(resourceUri);
+		if (inputAADLDirectory.exists() && inputAADLDirectory.isDirectory()) {
+			
+			outputJSONDirectory.mkdirs();
+			Arrays.asList(inputAADLDirectory.listFiles()).stream()
+					.filter(f -> f.getName().endsWith(".xmi"))
+					.forEachOrdered(this::transformOpenApiXmiToJsonFile);
+			
+			return 0;
+			
+		} else {
+			return 1;
+		}
 		
-		OpenAPIExporter exporter = ExporterBuilder.create();
-		JsonObject jsonOb = exporter.toJson(root.getApi());
+	}
+	
+	private void transformOpenApiXmiToJsonFile(File input) {
 		
-		System.out.println(jsonOb);
-		
-		FileWriter writer = new FileWriter(new File(outputJsonFilePath));
-		Gson gson = new Gson();
-		gson.toJson(jsonOb, writer);
-		writer.close();
-		
-		return 0;
+		try (InputStream aadlSource = new FileInputStream(input);) {
+			
+			URI resourceUri = URI.createURI(input.getPath());
+			Root root = ModelsService.retrieveOpenApiRootFromXmi(resourceUri);
+			
+			OpenAPIExporter exporter = ExporterBuilder.create();
+			JsonObject jsonOb = exporter.toJson(root.getApi());
+			
+			System.out.println(jsonOb);
+			
+			String newFileName = input.getName()
+					.replaceAll("\\.xmi", ".json");
+			
+			FileWriter writer = new FileWriter(new File(outputJSONDirectory,newFileName));
+			Gson gson = new Gson();
+			gson.toJson(jsonOb, writer);
+			writer.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
