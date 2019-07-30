ae6f26f7aae8d24985f9cd844a1ccd84b32cfc6a
cawal <ricardo.cawal@gmail.com>
2019-07-30 15:21:22 -0300

    Documentation for KT Deployment Model

--- framework/activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/wsdl/model.kt
+++ framework/activity-rest-framework/src/br/usp/ffclrp/dcm/lssb/activityrest/wsdl/model.kt
@@ -2,6 +2,19 @@ package br.usp.ffclrp.dcm.lssb.activityrest.wsdl
 
 import java.net.InetAddress
 
+/**
+ * This class presents the deployment-related information
+ * about a  Activity-REST service. This information are not
+ * provided by an AADL description as it is not inherently
+ * related to the activity the service provides, being only
+ * related to *how the service may be accessed*.
+ *
+ * @property protocol the protocol identifier.
+ * 						(Ex: http, https, ...)
+ * @property host the service host machine address
+ * @property port the service port
+ * @property basePath the base path for the service's root resource
+ */
 data class DeploymentModel(
     val protocol : String,
     val host : InetAddress,
