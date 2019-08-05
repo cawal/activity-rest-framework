87804008e110a1c83368eb4c7d287e657e2a9c3b
cawal <ricardo.cawal@gmail.com>
2019-08-05 10:54:27 -0300

    Removing Tycho from root

---
+++ transformations/activity-to-wsdl-cli/src/main/java/br/usp/ffclrp/dcm/lssb/activityrest/App.java
@@ -0,0 +1,13 @@
+package br.usp.ffclrp.dcm.lssb.activityrest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
