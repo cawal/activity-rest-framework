30edea4c41a916300fbc5b8022f9690d4f776c9c
cawal <ricardo.cawal@gmail.com>
2020-01-26 13:12:13 -0300

    inicio site

---
+++ bootstrap/src/br/usp/ffclrp/dcm/lssb/activityrest/bootstrap/rest/endpoints/RootResource.java
@@ -0,0 +1,37 @@
+package br.usp.ffclrp.dcm.lssb.activityrest.bootstrap.rest.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Root resource of the API.
+ * Provides the URI path segments for its subresources.
+ * 
+ * @author cawal
+ *
+ */
+@Path("/") 
+public class RootResource {
+	
+	@Context
+	Application app;
+	
+	@Context
+	UriInfo uriInfo;
+	
+	
+	
+	public RootResource() {	};
+	
+	@GET
+	@Produces("text/plain")
+	public Response ping() {
+		return Response.ok("pong").build();
+	}
+	
+}
