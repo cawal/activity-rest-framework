30ac46f46502c863111e7ac5641511fe59e36cdc
cawal <ricardo.cawal@gmail.com>
2020-02-29 10:52:09 -0300

    sync

--- clients/br.usp.ffclrp.dcm.lssb.activityrest.clients.generation/src/test/kotlin/br/usp/ffclrp/dcm/lssb/activityrest/clients/generation/BpmnToJbpmTest.kt
+++ clients/br.usp.ffclrp.dcm.lssb.activityrest.clients.generation/src/test/kotlin/br/usp/ffclrp/dcm/lssb/activityrest/clients/generation/BpmnToJbpmTest.kt
@@ -11,19 +11,22 @@ import org.eclipse.emf.common.util.URI as EmfUri
 import java.io.InputStream
 import java.io.File
 import br.usp.ffclrp.dcm.lssb.activityrest.clients.generation.galaxy.BpmnToJbpm
+import org.kie.api.io.ResourceType
+import br.usp.ffclrp.dcm.lssb.activityrest.jbpmclient.*
+import br.usp.ffclrp.dcm.lssb.activityrest.domain.ActivityInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BpmnToJbpmTest {
 
     private val transformationEngine = AadlToBpmn();
 
-    val activity by lazy {  
+    val activity by lazy {
         var inputStream = this::class.java.getResourceAsStream("./david.aadl")
         ModelsService.retrieveAADLModel(inputStream)
     }
-    
-    
-    val deployment by lazy {  
+
+
+    val deployment by lazy {
         var inputStream = this::class.java.getResourceAsStream("./david.deployment")
         ModelsService.retrieveDeploymentModel(inputStream)
     }
@@ -35,22 +38,44 @@ class BpmnToJbpmTest {
         val file = BpmnToJbpm().transform(bpmn);
 
         print(file)
+        executeFile(file);
 //        file.renameTo(File("/home/cawal/git/lssb/phd-tests-scratch-project/a.bpmn2"))
         assertAll("File exists",
                 { assertNotNull(file, "Returned a null File instance.") },
-                { assertTrue(file.exists(), "BPMN file does not exists") },
+//                { assertTrue(file.exists(), "BPMN file does not exists") },
                 { assertEquals("bpmn2", file.extension, "Wrong extension!") },
                 {
                     assertTrue(isValidBpmnFile(file))
                 }
+
         )
     }
 
-    fun isValidBpmnFile(file : File) : Boolean{
+    fun executeFile(file: File) {
+        val serviceRootUri = URI("http://dummy.uri")
+        val bpmnResources = listOf(BpmnProcessResource(
+                "david-chart-report",
+                file.getAbsolutePath(),
+                ResourceType.BPMN2
+        ))
+
+        val executedProcessId = "david-chart-report"
+        val dryRun: Boolean = true
+
+        val service = ActivityRestService(serviceRootUri,
+                bpmnResources,
+                executedProcessId,
+                dryRun)
+
+        service.execute(ActivityInstance())
+
+    }
+
+    fun isValidBpmnFile(file: File): Boolean {
         val bpmnObject = ModelsService.retrieveBPMN(
-                	EmfUri.createFileURI(file.getAbsolutePath())
+                EmfUri.createFileURI(file.getAbsolutePath())
         )
-        
+
         return bpmnObject != null
     }
 
